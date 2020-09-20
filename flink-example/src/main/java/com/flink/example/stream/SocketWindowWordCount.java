15b4fa23118665f901562d71f5b4bf1f46842f8c
jifeng.si <jifeng.si@qunar.com>
2020-09-20 22:46:17 +0800

    MOD: Flink SocketWindowWordCount

---
+++ flink-example/src/main/java/com/flink/example/stream/SocketWindowWordCount.java
@@ -0,0 +1,74 @@
+package com.flink.example.stream;
+
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.common.functions.ReduceFunction;
+import org.apache.flink.api.java.functions.KeySelector;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.api.java.utils.ParameterTool;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.util.Collector;
+
+/**
+ * SocketWindowWordCount
+ * Created by wy on 2020/9/20.
+ */
+public class SocketWindowWordCount {
+    public static void main(String[] args) throws Exception {
+        // 1. 通过 ParameterTool 解析参数
+        final String hostname;
+        final int port;
+        try {
+            final ParameterTool params = ParameterTool.fromArgs(args);
+            hostname = params.has("hostname") ? params.get("hostname") : "localhost";
+            port = params.getInt("port");
+        }
+        catch (Exception e) {
+            System.err.println("No port specified. Please run 'SocketWindowWordCount " +
+                    "--hostname <hostname> --port <port>', where hostname (localhost by default) " +
+                    "and port is the address of the text server");
+            System.err.println("To start a simple text server, run 'netcat -l <port>' and " +
+                    "type the input text into the command line");
+            return;
+        }
+
+        // 2. 创建StreamExecutionEnvironment
+        final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+        // 3. 连接Socket获取数据
+        DataStream<String> text = env.socketTextStream(hostname, port, "\n");
+
+        // 4. 输入字符串解析为<单词,出现次数>
+        DataStream<Tuple2<String, Integer>> wordsCount = text.flatMap(new FlatMapFunction<String, Tuple2<String, Integer>>() {
+            @Override
+            public void flatMap(String value, Collector out) {
+                for (String word : value.split("\\s")) {
+                    out.collect(Tuple2.of(word, 1));
+                }
+            }
+        });
+
+        // 5. 分组窗口计算
+        DataStream<Tuple2<String, Integer>> windowCount = wordsCount
+                .keyBy(new KeySelector<Tuple2<String, Integer>, String>() {
+                    @Override
+                    public String getKey(Tuple2<String, Integer> tuple) throws Exception {
+                        return tuple.f0;
+                    }
+                })
+                .timeWindow(Time.seconds(5))
+                .reduce(new ReduceFunction<Tuple2<String, Integer>>() {
+                    @Override
+                    public Tuple2 reduce(Tuple2<String, Integer> a, Tuple2<String, Integer> b) {
+                        return new Tuple2(a.f0, a.f1 + b.f1);
+                    }
+                });
+
+        // 6. 输出结果并开始执行
+        windowCount.print().setParallelism(1);
+
+        // 7. 开启作业
+        env.execute("Socket Window WordCount");
+    }
+}
