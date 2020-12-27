77cc59894149b70c6242c42c578225b14ef58adb
jifeng.si <jifeng.si@qunar.com>
2020-12-27 22:47:16 +0800

    MOD: SavepointExample

--- flink-example/src/main/java/com/flink/example/stream/state/savepoint/SavepointExample.java
+++ flink-example/src/main/java/com/flink/example/stream/state/savepoint/SavepointExample.java
@@ -1,14 +1,15 @@
 package com.flink.example.stream.state.savepoint;
 
-import com.flink.example.stream.function.MyStatefulMapFunction;
-import com.flink.example.stream.source.SimpleCustomSource;
+import org.apache.flink.api.common.functions.FlatMapFunction;
 import org.apache.flink.api.common.restartstrategy.RestartStrategies;
 import org.apache.flink.api.java.functions.KeySelector;
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.runtime.state.StateBackend;
 import org.apache.flink.runtime.state.filesystem.FsStateBackend;
+import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.CheckpointConfig;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.util.Collector;
 
 /**
  * Savepoint Example
@@ -22,15 +23,27 @@ public class SavepointExample {
         env.enableCheckpointing(1000L);
         env.getCheckpointConfig().enableExternalizedCheckpoints(CheckpointConfig.ExternalizedCheckpointCleanup.RETAIN_ON_CANCELLATION);
 
-        env.addSource(new SimpleCustomSource()).uid("simple-custom-source") // 为Source指定ID
+        DataStream<String> source = env.socketTextStream("localhost", 9100, "\n")
+                .name("MySourceFunction").uid("source-uid");
+        DataStream<Tuple2<String, Integer>> wordsCount = source.flatMap(new FlatMapFunction<String, Tuple2<String, Integer>>() {
+            @Override
+            public void flatMap(String value, Collector out) {
+                for (String word : value.split("\\s")) {
+                    out.collect(Tuple2.of(word, 1));
+                }
+            }
+        }).name("MyFlatMapFunction").uid("flatMap-uid");
+
+        DataStream<Tuple2<String, Integer>> windowCount = wordsCount
                 .keyBy(new KeySelector<Tuple2<String, Integer>, String>() {
                     @Override
-                    public String getKey(Tuple2<String, Integer> value) throws Exception {
-                        return value.getField(0);
+                    public String getKey(Tuple2<String, Integer> tuple) throws Exception {
+                        return tuple.f0;
                     }
                 })
-                .map(new MyStatefulMapFunction()).uid("my-stateful-map-function") // 为Map指定ID
-                .print(); // 自动生成ID
+                .sum(1).name("MySumFunction").uid("sum-uid");
+
+        windowCount.print().name("MyPrintFunction");
         env.execute("SavepointExample");
     }
 }
