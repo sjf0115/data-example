e51dc497f0c6f53f18e2793eca4e9c22efdc7fc3
jifeng.si <jifeng.si@qunar.com>
2021-01-06 09:54:53 +0800

    MOD: WatermarkDeprecatedExample

---
+++ common-example/src/main/java/com/common/example/utils/DateUtil.java
@@ -0,0 +1,104 @@
+package com.common.example.utils;
+
+import org.apache.commons.lang3.StringUtils;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Objects;
+
+/**
+ * 时间工具类
+ * Created by wy on 2021/1/5.
+ */
+public class DateUtil {
+
+    /**
+     * 时间字符串转时间戳
+     *
+     * @param dateStr 字符串时间
+     * @param format 时间格式
+     * @return
+     */
+    public static Long date2TimeStamp(String dateStr, String format) throws ParseException {
+        SimpleDateFormat sdf = new SimpleDateFormat(format);
+        return sdf.parse(dateStr).getTime();
+    }
+
+    /**
+     * 时间 格式化 yyyy-MM-dd
+     *
+     * @param dateStr 字符串时间
+     * @param format 时间格式
+     * @return
+     */
+    public static String dateFormat(String dateStr, String format) throws ParseException {
+        Date date = new SimpleDateFormat(format).parse(dateStr);
+        String dateFormat = new SimpleDateFormat("yyyy-MM-dd").format(date);
+        return dateFormat;
+    }
+
+    /**
+     * 时间 格式化
+     *
+     * @param dateStr 时间
+     * @param sourceFormat 源时间格式
+     * @param targetFormat 目标时间格式
+     * @return
+     */
+    public static String dateFormat(String dateStr, String sourceFormat, String targetFormat) throws ParseException {
+        Date date = new SimpleDateFormat(sourceFormat).parse(dateStr);
+        String dateFormat = new SimpleDateFormat(targetFormat).format(date);
+        return dateFormat;
+    }
+
+    /**
+     * 时间 格式化
+     *
+     * @param dateSource
+     * @param format 目标格式化
+     * @return
+     */
+    public static String dateFormat(Date dateSource, String format) {
+        if (Objects.equals(dateSource, null) || StringUtils.isBlank(format)) {
+            return null;
+        }
+        SimpleDateFormat sdf = new SimpleDateFormat(format);
+        String dateFormat = sdf.format(dateSource);
+        return dateFormat;
+    }
+
+    public static String timeStamp2Date(Long timeStamp, String formats){
+        String date = new SimpleDateFormat(formats).format(new Date(timeStamp));
+        return date;
+    }
+
+    /**
+     * 星期
+     * @param date
+     * @return
+     * @throws ParseException
+     */
+    public static String getWeek(String date) throws ParseException {
+        SimpleDateFormat format = new SimpleDateFormat("yyyyMMdd");
+        Date dt = format.parse(date);
+        return  getWeek(dt);
+    }
+
+    /**
+     * 星期
+     * @param date
+     * @return
+     */
+    public static String getWeek(Date date) {
+        String[] weekDays = {"星期天", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六"};
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(date);
+        int w = cal.get(Calendar.DAY_OF_WEEK) - 1;
+        if (w < 0){
+            w = 0;
+        }
+        return weekDays[w];
+    }
+}
---
+++ flink-example/src/main/java/com/flink/example/stream/watermark/WatermarkDeprecatedExample.java
@@ -0,0 +1,111 @@
+package com.flink.example.stream.watermark;
+
+import com.common.example.utils.DateUtil;
+import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.api.common.restartstrategy.RestartStrategies;
+import org.apache.flink.api.java.functions.KeySelector;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.runtime.state.StateBackend;
+import org.apache.flink.runtime.state.filesystem.FsStateBackend;
+import org.apache.flink.streaming.api.TimeCharacteristic;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.CheckpointConfig;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.AssignerWithPeriodicWatermarks;
+import org.apache.flink.streaming.api.watermark.Watermark;
+import org.apache.flink.streaming.api.windowing.time.Time;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.annotation.Nullable;
+
+/**
+ * Watermark @Deprecated from 1.11
+ * Created by wy on 2021/1/4.
+ */
+public class WatermarkDeprecatedExample {
+
+    private static final Logger LOG = LoggerFactory.getLogger(WatermarkDeprecatedExample.class);
+
+    public static void main(String[] args) throws Exception {
+        final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setStateBackend((StateBackend) new FsStateBackend("hdfs://localhost:9000/flink/checkpoints"));
+        env.setRestartStrategy(RestartStrategies.fixedDelayRestart(3,1000));
+        env.enableCheckpointing(1000L);
+        env.getCheckpointConfig().enableExternalizedCheckpoints(CheckpointConfig.ExternalizedCheckpointCleanup.RETAIN_ON_CANCELLATION);
+
+        // 设置事件时间特性
+        env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime);
+
+        DataStream<String> source = env.socketTextStream("localhost", 9100, "\n");
+
+        DataStream<Tuple2<String, Long>> input = source
+                .map(new MapFunction<String, Tuple2<String, Long>>() {
+                    @Override
+                    public Tuple2<String, Long> map(String str) throws Exception {
+                        LOG.info("[INFO] element: {}", str);
+                        String[] params = str.split(",");
+                        String message = params[0];
+                        String time = params[1];
+                        Long timeStamp = DateUtil.date2TimeStamp(time, "yyyy-MM-dd HH:mm:ss");
+                        return new Tuple2<>(message, timeStamp);
+                    }
+                });
+
+        DataStream<Tuple2<String, Long>> result = input.assignTimestampsAndWatermarks(new BoundedOutOfOrdernessGenerator())
+                // 转换
+                .map(new MapFunction<Tuple2<String,Long>, Tuple2<String, Long>>() {
+                    @Override
+                    public Tuple2<String, Long> map(Tuple2<String, Long> value) throws Exception {
+                        return new Tuple2<String, Long>(value.f0, 1L);
+                    }
+                })
+                // 分组
+                .keyBy(new KeySelector<Tuple2<String, Long>, String>() {
+                    @Override
+                    public String getKey(Tuple2<String, Long> tuple) throws Exception {
+                        return tuple.f0;
+                    }
+                })
+                // 窗口
+                .timeWindow(Time.minutes(10), Time.minutes(5))
+                // 求和
+                .sum(1);
+
+        result.print();
+        env.execute("WatermarkDeprecatedExample");
+    }
+
+    public static class BoundedOutOfOrdernessGenerator implements AssignerWithPeriodicWatermarks<Tuple2<String, Long>> {
+
+        private final Long outOfOrdernessMillis = 600000L;
+        private Long currentMaxTimeStamp = Long.MIN_VALUE + outOfOrdernessMillis + 1;
+
+        // 默认100ms被调用一次
+        @Nullable
+        @Override
+        public Watermark getCurrentWatermark() {
+            return new Watermark(currentMaxTimeStamp - outOfOrdernessMillis - 1);
+        }
+
+        @Override
+        public long extractTimestamp(Tuple2<String, Long> element, long recordTimestamp) {
+
+            Watermark watermark = getCurrentWatermark();
+            LOG.info("[INFO] watermark: {}", DateUtil.timeStamp2Date(watermark.getTimestamp(), "yyyy-MM-dd HH:mm:ss")+ " | " + watermark.getTimestamp() + "]");
+            String key = element.f0;
+            Long timestamp = element.f1;
+
+            currentMaxTimeStamp = Math.max(timestamp, currentMaxTimeStamp);
+
+            LOG.info("[INFO] timestamp, key: {}, eventTime: {}, currentMaxTimeStamp: {}, maxOutOfOrderness: {}",
+                    key,
+                    "[" + DateUtil.timeStamp2Date(timestamp, "yyyy-MM-dd HH:mm:ss") + " | " + timestamp + "]",
+                    "[" + DateUtil.timeStamp2Date(currentMaxTimeStamp, "yyyy-MM-dd HH:mm:ss")+" | "+currentMaxTimeStamp + "]",
+                    outOfOrdernessMillis
+            );
+
+            return timestamp;
+        }
+    }
+}
