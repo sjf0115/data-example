7cf8fa72deabe8578a35549f6988d7f894fc3778
jifeng.si <jifeng.si@qunar.com>
2021-02-16 23:55:43 +0800

    MOD: AggregateFunctionExample

--- flink-example/src/main/java/com/flink/example/stream/window/WindowFunctionExample.java
+++ flink-example/src/main/java/com/flink/example/stream/window/WindowFunctionExample.java
@@ -1,7 +1,7 @@
 package com.flink.example.stream.window;
 
 import com.common.example.utils.DateUtil;
-import org.apache.flink.api.common.functions.AggregateFunction;
+import com.google.common.collect.Lists;
 import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.api.common.restartstrategy.RestartStrategies;
 import org.apache.flink.api.java.functions.KeySelector;
@@ -14,17 +14,21 @@ import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.CheckpointConfig;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.functions.timestamps.BoundedOutOfOrdernessTimestampExtractor;
+import org.apache.flink.streaming.api.functions.windowing.WindowFunction;
 import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.streaming.api.windowing.windows.TimeWindow;
+import org.apache.flink.util.Collector;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.List;
+
 /**
- * AggregateFunction Example
- * 实现功能：分组求平均值
- * Created by wy on 2021/2/12.
+ * WindowFunction Example
+ * Created by wy on 2021/2/16.
  */
-public class AggregateFunctionExample {
-    private static final Logger LOG = LoggerFactory.getLogger(AggregateFunctionExample.class);
+public class WindowFunctionExample {
+    private static final Logger LOG = LoggerFactory.getLogger(AggregateProcessWindowFunctionExample.class);
 
     public static void main(String[] args) throws Exception {
         final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
@@ -38,7 +42,7 @@ public class AggregateFunctionExample {
 
         DataStream<String> source = env.socketTextStream("localhost", 9100, "\n");
 
-        // Stream of (key, timestamp, count)
+        // Stream of (key, timestamp)
         DataStream<Tuple3<String, Long, Integer>> stream = source.map(new MapFunction<String, Tuple3<String, Long, Integer>>() {
             @Override
             public Tuple3<String, Long, Integer> map(String str) throws Exception {
@@ -46,13 +50,13 @@ public class AggregateFunctionExample {
                 String key = params[0];
                 String time = params[1];
                 Long timeStamp = DateUtil.date2TimeStamp(time, "yyyy-MM-dd HH:mm:ss");
-                Integer count = Integer.parseInt(params[2]);
-                LOG.info("[Element] Key: " + key + ", timeStamp: [" + timeStamp + "|" + time + "], Count: " + count);
-                return new Tuple3(key, timeStamp, count);
+                LOG.info("[Element] Key: " + key + ", timeStamp: [" + timeStamp + "|" + time + "]");
+                return new Tuple3(key, timeStamp, 1);
             }
         });
 
-        DataStream<Tuple2<String, Double>> result = stream
+        // 滚动窗口
+        DataStream<Tuple2<String, Integer>> result = stream
                 // 提取时间戳与设置Watermark
                 .assignTimestampsAndWatermarks(new BoundedOutOfOrdernessTimestampExtractor<Tuple3<String, Long, Integer>>(Time.minutes(10)) {
                     @Override
@@ -60,69 +64,62 @@ public class AggregateFunctionExample {
                         return element.f1;
                     }
                 })
-                // 格式转换
-                .map(new MapFunction<Tuple3<String,Long,Integer>, Tuple2<String, Integer>>() {
-                    @Override
-                    public Tuple2<String, Integer> map(Tuple3<String, Long, Integer> value) throws Exception {
-                        return new Tuple2<String, Integer>(value.f0, value.f2);
-                    }
-                })
                 // 分组
-                .keyBy(new KeySelector<Tuple2<String, Integer>, String>() {
+                .keyBy(new KeySelector<Tuple3<String, Long, Integer>, String>() {
                     @Override
-                    public String getKey(Tuple2<String, Integer> value) throws Exception {
+                    public String getKey(Tuple3<String, Long, Integer> value) throws Exception {
                         return value.f0;
                     }
                 })
                 // 窗口大小为10分钟、滑动步长为5分钟的滑动窗口
                 .timeWindow(Time.minutes(10), Time.minutes(5))
-                .aggregate(new AverageAggregateFunction());
+                .apply(new MyWindowFunction());
 
         result.print();
-        env.execute("AggregateFunctionExample");
+        env.execute("WindowFunctionExample");
     }
 
     /**
-     * 自定义AggregateFunction
+     * 自定义实现WindowFunction
      */
-    private static class AverageAggregateFunction implements AggregateFunction<Tuple2<String, Integer>, Tuple3<String, Long, Long>, Tuple2<String, Double>> {
-
-        // IN：Tuple2<String, Long>
-        // ACC：Tuple3<String, Long, Long> -> <Key, Sum, Count>
-        // OUT：Tuple2<String, Double>
-
+    private static class MyWindowFunction implements WindowFunction<Tuple3<String, Long, Integer>, Tuple2<String, Integer>, String, TimeWindow> {
         @Override
-        public Tuple3<String, Long, Long> createAccumulator() {
-            return new Tuple3<String, Long, Long>("", 0L, 0L);
-        }
+        public void apply(String key, TimeWindow window, Iterable<Tuple3<String, Long, Integer>> input, Collector<Tuple2<String, Integer>> out) throws Exception {
+            int count = 0;
+            List<String> list = Lists.newArrayList();
+            for (Tuple3<String, Long, Integer> element : input) {
+                list.add(element.f0 + "|" + element.f1 + "|" + DateUtil.timeStamp2Date(element.f1, "yyyy-MM-dd HH:mm:ss"));
+                Integer value = element.f2;
+                count += value;
+            }
 
-        @Override
-        public Tuple3<String, Long, Long> add(Tuple2<String, Integer> value, Tuple3<String, Long, Long> accumulator) {
-            return new Tuple3<String, Long, Long>(value.f0, accumulator.f1 + value.f1, accumulator.f2 + 1L);
-        }
+            // 窗口元信息相对ProcessWindowFunction较少
+            long start = window.getStart();
+            long end = window.getEnd();
+            String startTime = DateUtil.timeStamp2Date(start, "yyyy-MM-dd HH:mm:ss");
+            String endTime = DateUtil.timeStamp2Date(end, "yyyy-MM-dd HH:mm:ss");
 
-        @Override
-        public Tuple2<String, Double> getResult(Tuple3<String, Long, Long> accumulator) {
-            return new Tuple2<String, Double>(accumulator.f0, ((double) accumulator.f1) / accumulator.f2);
-        }
+            StringBuilder sb = new StringBuilder();
+            sb.append("[Window] Key: " + list.toString());
+            sb.append(", Window：[" + startTime + ", " + endTime + "]");
+            sb.append(", Count: " + count);
+            LOG.info(sb.toString());
 
-        @Override
-        public Tuple3<String, Long, Long> merge(Tuple3<String, Long, Long> a, Tuple3<String, Long, Long> b) {
-            return new Tuple3<String, Long, Long>(a.f0, a.f1 + b.f1, a.f2 + b.f2);
+            out.collect(new Tuple2<>(key, count));
         }
     }
-    // 输入样例
-//    A,2021-02-14 12:07:01,9
-//    B,2021-02-14 12:08:01,5
-//    A,2021-02-14 12:14:01,3
-//    C,2021-02-14 12:09:01,2
-//    C,2021-02-14 12:15:01,5
-//    A,2021-02-14 12:08:01,4
-//    B,2021-02-14 12:13:01,6
-//    B,2021-02-14 12:21:01,1
-//    D,2021-02-14 12:04:01,3
-//    B,2021-02-14 12:26:01,2
-//    B,2021-02-14 12:17:01,7
-//    D,2021-02-14 12:09:01,8
-//    C,2021-02-14 12:30:01,1
+//    // 输入样例
+//    A,2021-02-14 12:07:01
+//    B,2021-02-14 12:08:01
+//    A,2021-02-14 12:14:01
+//    C,2021-02-14 12:09:01
+//    C,2021-02-14 12:15:01
+//    A,2021-02-14 12:08:01
+//    B,2021-02-14 12:13:01
+//    B,2021-02-14 12:21:01
+//    D,2021-02-14 12:04:01
+//    B,2021-02-14 12:26:01
+//    B,2021-02-14 12:17:01
+//    D,2021-02-14 12:09:01
+//    C,2021-02-14 12:30:01
 }
