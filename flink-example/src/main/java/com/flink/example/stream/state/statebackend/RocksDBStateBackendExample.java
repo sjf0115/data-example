348f63f4a3599798994e710da3b5e71c3e908881
jifeng.si <jifeng.si@qunar.com>
2021-08-01 14:21:17 +0800

    MOD: rocksdb backend

--- flink-example/src/main/java/com/flink/example/stream/state/statebackend/RocksDBStateBackendExample.java
+++ flink-example/src/main/java/com/flink/example/stream/state/statebackend/RocksDBStateBackendExample.java
@@ -1,41 +1,42 @@
-package com.flink.example.stream.state.state;
+package com.flink.example.stream.state.statebackend;
 
 import com.common.example.utils.DateUtil;
 import org.apache.flink.api.common.functions.MapFunction;
-import org.apache.flink.api.common.state.StateTtlConfig;
-import org.apache.flink.api.common.time.Time;
+import org.apache.flink.api.common.restartstrategy.RestartStrategies;
 import org.apache.flink.api.java.tuple.Tuple3;
+import org.apache.flink.runtime.state.StateBackend;
+import org.apache.flink.runtime.state.filesystem.FsStateBackend;
+import org.apache.flink.streaming.api.TimeCharacteristic;
 import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.CheckpointConfig;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * 功能：状态过期
+ * 功能：
  * 作者：SmartSi
  * 博客：http://smartsi.club/
  * 公众号：大数据生态
- * 日期：2021/6/26 下午12:46
+ * 日期：2021/7/11 下午9:19
  */
-public class StateTTLExample {
+public class RocksDBStateBackendExample {
 
-    private static final Logger LOG = LoggerFactory.getLogger(StateTTLExample.class);
+    private static final Logger LOG = LoggerFactory.getLogger(RocksDBStateBackendExample.class);
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setStateBackend(null);
+
+        env.setStateBackend((StateBackend) new FsStateBackend("hdfs://localhost:9000/flink/checkpoints"));
+        env.setRestartStrategy(RestartStrategies.fixedDelayRestart(3,1000));
+        env.enableCheckpointing(1000L);
+        env.getCheckpointConfig().enableExternalizedCheckpoints(CheckpointConfig.ExternalizedCheckpointCleanup.RETAIN_ON_CANCELLATION);
 
         // 设置事件时间特性
+        env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime);
 
         DataStream<String> source = env.socketTextStream("localhost", 9100, "\n");
-
-        // TTL 配置
-        StateTtlConfig ttlConfig = StateTtlConfig
-                .newBuilder(Time.minutes(1))
-                .setUpdateType(StateTtlConfig.UpdateType.OnCreateAndWrite)
-                .setStateVisibility(StateTtlConfig.StateVisibility.NeverReturnExpired)
-                .build();
-
-
 
         // Stream of (key, timestamp)
         DataStream<Tuple3<String, Long, Integer>> stream = source.map(new MapFunction<String, Tuple3<String, Long, Integer>>() {
