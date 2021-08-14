d85f9ef7d96448c4d191da047c06ab75078e90c1
jifeng.si <jifeng.si@qunar.com>
2021-08-14 10:49:11 +0800

    MOD: flink kafka connector

--- flink-example/src/main/java/com/flink/example/sql/ddl/KafkaConnectorExample.sql
+++ flink-example/src/main/java/com/flink/example/sql/ddl/KafkaConnectorExample.sql
@@ -1,17 +1,50 @@
 
 
 -- Source
-CREATE TABLE KafkaTable (
+CREATE TABLE kafka_source_table (
   uid STRING COMMENT '用户Id',
   wid STRING COMMENT '微博Id',
   tm STRING COMMENT '发微博时间',
-  content STRING COMMENT '微博内容',
-  ts TIMESTAMP(3) METADATA FROM 'timestamp'
+  content STRING COMMENT '微博内容'
+) WITH (
+  'connector' = 'kafka',
+  'topic' = 'behavior',
+  'properties.bootstrap.servers' = 'localhost:9092',
+  'properties.group.id' = 'print-example',
+  'scan.startup.mode' = 'earliest-offset',
+  'value.format' = 'json',
+  'value.json.ignore-parse-errors' = 'true'
+);
+
+CREATE TABLE kafka_meta_source_table (
+  uid STRING COMMENT '用户Id',
+  wid STRING COMMENT '微博Id',
+  tm STRING COMMENT '发微博时间',
+  content STRING COMMENT '微博内容'
+) WITH (
+  'connector' = 'kafka',
+  'topic' = 'behavior',
+  'properties.bootstrap.servers' = 'localhost:9092',
+  'properties.group.id' = 'kafka-example',
+  'scan.startup.mode' = 'earliest-offset',
+  'value.format' = 'json',
+  'value.json.ignore-parse-errors' = 'true'
+);
+
+
+CREATE TABLE KafkaTable (
+  `event_time` TIMESTAMP(3) METADATA FROM 'value.source.timestamp' VIRTUAL,  -- from Debezium format
+  `origin_table` STRING METADATA FROM 'value.source.table' VIRTUAL, -- from Debezium format
+  `partition_id` BIGINT METADATA FROM 'partition' VIRTUAL,  -- from Kafka connector
+  `offset` BIGINT METADATA VIRTUAL,  -- from Kafka connector
+  `user_id` BIGINT,
+  `item_id` BIGINT,
+  `behavior` STRING
 ) WITH (
   'connector' = 'kafka',
   'topic' = 'user_behavior',
   'properties.bootstrap.servers' = 'localhost:9092',
   'properties.group.id' = 'testGroup',
   'scan.startup.mode' = 'earliest-offset',
-  'format' = 'csv'
-)
+  'value.format' = 'debezium-json'
+);
