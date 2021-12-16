a7fe3ea68387265e7c2ceb4f7972da34c443bc06
jifeng.si <jifeng.si@qunar.com>
2021-12-16 15:28:57 +0800

    MOD: RowFormatFileSinkExample

--- flink-example/src/main/java/com/flink/example/stream/connector/file/sink/RowFormatFileSinkExample.java
+++ flink-example/src/main/java/com/flink/example/stream/connector/file/sink/RowFormatFileSinkExample.java
@@ -1,16 +1,16 @@
-package com.flink.example.stream.sink;
-
+package com.flink.example.stream.connector.file.sink;
 import org.apache.flink.api.common.serialization.SimpleStringEncoder;
 import org.apache.flink.connector.file.sink.FileSink;
 import org.apache.flink.core.fs.Path;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.sink.filesystem.bucketassigners.DateTimeBucketAssigner;
 import org.apache.flink.streaming.api.functions.sink.filesystem.rollingpolicies.DefaultRollingPolicy;
 
 import java.util.concurrent.TimeUnit;
 
 /**
- * 功能：File Sink RowFormat
+ * 功能：RowFormat File Sink
  * 作者：SmartSi
  * 博客：http://smartsi.club/
  * 公众号：大数据生态
@@ -23,9 +23,13 @@ public class RowFormatFileSinkExample {
 
         DataStream<String> source = env.socketTextStream("localhost", 9100, "\n");
 
-        String outputPath = "hdfs://localhost:9000/flink/file";
+        String outputPath = "hdfs://localhost:9000/flink/connector/file/row/";
         final FileSink<String> sink = FileSink
                 .forRowFormat(new Path(outputPath), new SimpleStringEncoder<String>("UTF-8"))
+                // 生成 Bucket
+                .withBucketAssigner(new DateTimeBucketAssigner<>("yyyyMMdd-HH"))
+                .withBucketCheckInterval(60L * 1000L)
+                // 滚动策略
                 .withRollingPolicy(
                         DefaultRollingPolicy.builder()
                                 // 包含至少 15 分钟的数据
