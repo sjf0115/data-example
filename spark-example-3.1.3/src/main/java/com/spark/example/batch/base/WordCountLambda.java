64350d57e7d0c14ef2e508660a772fa89890b69c
jifeng.si <jifeng.si@qunar.com>
2022-03-28 21:48:42 +0800

    MOD: Spark WordCount Lambda

---
+++ spark-example-3.1.3/src/main/java/com/spark/example/batch/base/WordCountLambda.java
@@ -0,0 +1,48 @@
+package com.spark.example.batch.base;
+
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.sql.SparkSession;
+import scala.Tuple2;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Pattern;
+
+/**
+ * 功能：WordCount Lambda
+ * 作者：SmartSi
+ * 博客：http://smartsi.club/
+ * 公众号：大数据生态
+ * 日期：2022/3/28 下午9:47
+ */
+public class WordCountLambda {
+    private static final Pattern SPACE = Pattern.compile(" ");
+
+    public static void main(String[] args) throws Exception {
+
+        if (args.length < 1) {
+            System.err.println("Usage: JavaWordCount <file>");
+            System.exit(1);
+        }
+
+        SparkSession spark = SparkSession
+                .builder()
+                .appName("JavaWordCount")
+                .getOrCreate();
+
+        JavaRDD<String> lines = spark.read().textFile(args[0]).javaRDD();
+
+        JavaRDD<String> words = lines.flatMap(s -> Arrays.asList(SPACE.split(s)).iterator());
+
+        JavaPairRDD<String, Integer> ones = words.mapToPair(s -> new Tuple2<>(s, 1));
+
+        JavaPairRDD<String, Integer> counts = ones.reduceByKey((i1, i2) -> i1 + i2);
+
+        List<Tuple2<String, Integer>> output = counts.collect();
+        for (Tuple2<?,?> tuple : output) {
+            System.out.println(tuple._1() + ": " + tuple._2());
+        }
+        spark.stop();
+    }
+}
