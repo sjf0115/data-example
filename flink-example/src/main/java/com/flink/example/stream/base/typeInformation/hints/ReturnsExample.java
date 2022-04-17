c7ae833af7d59b2c61fa9dfc0eaf504d4a374233
sjf0115 <1203745031@qq.com>
2022-04-17 23:04:00 +0800

    MOD: Returns 示例

--- flink-example/src/main/java/com/flink/example/stream/base/typeInformation/hints/ReturnsExample.java
+++ flink-example/src/main/java/com/flink/example/stream/base/typeInformation/hints/ReturnsExample.java
@@ -1,6 +1,7 @@
 package com.flink.example.stream.base.typeInformation.hints;
 
 import com.common.example.bean.WordCount;
+import org.apache.flink.api.common.typeinfo.TypeHint;
 import org.apache.flink.api.common.typeinfo.TypeInformation;
 import org.apache.flink.api.common.typeinfo.Types;
 import org.apache.flink.api.java.tuple.Tuple2;
@@ -19,21 +20,33 @@ public class ReturnsExample {
     public static void main(String[] args) throws Exception {
         final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 
-        // 示例1
-        DataStream<Tuple2<String, Integer>> result1 = env.fromElements("a", "b", "a")
-                .map(value -> Tuple2.of(value, 1))
-                .returns(Types.TUPLE(Types.STRING, Types.INT));
-        result1.print("R1");
-
-        // 示例2
-        DataStream<WordCount> result2 = env.fromElements("a b a")
+        // 示例1 非泛型类型 直接传入 Class
+        DataStream<WordCount> result1 = env.fromElements("a b a")
                 .flatMap((String value, Collector<WordCount> out) -> {
                     for(String word : value.split("\\s")) {
                         out.collect(new WordCount(word, 1));
                     }
                 })
-                .returns(TypeInformation.of(WordCount.class));
+                .returns(WordCount.class);
+        result1.print("R1");
+
+        // 示例2 泛型类型 优先推荐借助 TypeHint
+        DataStream<Tuple2<String, Integer>> result2 = env.fromElements("a", "b", "a")
+                .map(value -> Tuple2.of(value, 1))
+                .returns(new TypeHint<Tuple2<String, Integer>>() {});
         result2.print("R2");
+
+        // 示例3 TypeInformation.of + TypeHint
+        DataStream<Tuple2<String, Integer>> result3 = env.fromElements("a", "b", "a")
+                .map(value -> Tuple2.of(value, 1))
+                .returns(TypeInformation.of(new TypeHint<Tuple2<String, Integer>>() {}));
+        result3.print("R3");
+
+        // 示例4 Types 快捷方式
+        DataStream<Tuple2<String, Integer>> result4 = env.fromElements("a", "b", "a")
+                .map(value -> Tuple2.of(value, 1))
+                .returns(Types.TUPLE(Types.STRING, Types.INT));
+        result4.print("R4");
 
         env.execute("ReturnsExample");
     }
