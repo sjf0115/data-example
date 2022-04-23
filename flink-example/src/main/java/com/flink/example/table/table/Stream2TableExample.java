925083c61d3ac8373a77ab9e309cb2621b9a160f
sjf0115 <1203745031@qq.com>
2022-04-23 18:05:26 +0800

    MOD: Stream2TableExample 修改 bean

--- flink-example/src/main/java/com/flink/example/table/table/Stream2TableExample.java
+++ flink-example/src/main/java/com/flink/example/table/table/Stream2TableExample.java
@@ -1,12 +1,11 @@
 package com.flink.example.table.table;
 
+import com.common.example.bean.WordCount;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.table.api.Table;
 import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
 import org.apache.flink.types.Row;
-
-import static org.apache.flink.table.api.Expressions.$;
 
 /**
  * 功能：Stream 转换为 Table 示例
@@ -22,29 +21,39 @@ public class Stream2TableExample {
         StreamTableEnvironment tEnv = StreamTableEnvironment.create(env);
 
         // 创建 DataStream
-        DataStream<Row> dataStream = env.fromElements(
+
+        // 示例1 fromDataStream()
+        DataStream<WordCount> sourceStream1 = env.fromElements(
+                new WordCount("hello", 1),
+                new WordCount("word", 4),
+                new WordCount("hello", 1));
+        //Table table1 = tEnv.fromDataStream(sourceStream1, $("word"), $("frequency"));
+        Table table1 = tEnv.fromDataStream(sourceStream1);
+        table1.printSchema();
+        DataStream<WordCount> resultStream1 = tEnv.toAppendStream(table1, WordCount.class);
+        resultStream1.print("R1");
+
+        // 示例2 createTemporaryView()
+        DataStream<WordCount> sourceStream2 = env.fromElements(
+                new WordCount("hello", 1),
+                new WordCount("word", 4),
+                new WordCount("hello", 1));
+        tEnv.createTemporaryView("input_table", sourceStream2);
+        //tEnv.createTemporaryView("input_table", sourceStream2, $("word"), $("frequency"));
+        Table table2 = tEnv.from("input_table");
+        table2.printSchema();
+        DataStream<WordCount> resultStream2 = tEnv.toAppendStream(table2, WordCount.class);
+        resultStream2.print("R2");
+
+        // 示例3 fromChangelogStream
+        DataStream<Row> sourceStream3 = env.fromElements(
                 Row.of("hello", 1),
                 Row.of("word", 4),
                 Row.of("hello", 1));
-
-        // 示例1 fromDataStream()
-        Table table1 = tEnv.fromDataStream(dataStream).as("word", "count");
-        table1.printSchema();
-        DataStream<Row> stream1 = tEnv.toChangelogStream(table1);
-        stream1.print("R1");
-
-        // 示例2 createTemporaryView()
-        tEnv.createTemporaryView("input_table", dataStream, $("word"), $("count"));
-        Table table2 = tEnv.from("input_table");
-        table2.printSchema();
-        DataStream<Row> stream2 = tEnv.toChangelogStream(table2);
-        stream2.print("R2");
-
-        // 示例3 fromChangelogStream
-        Table table3 = tEnv.fromChangelogStream(dataStream);
+        Table table3 = tEnv.fromChangelogStream(sourceStream3);
         table3.printSchema();
-        DataStream<Row> stream3 = tEnv.toChangelogStream(table3);
-        stream3.print("R3");
+        DataStream<Row> resultStream3 = tEnv.toChangelogStream(table3);
+        resultStream3.print("R3");
 
         // 执行
         env.execute();
@@ -52,22 +61,22 @@ public class Stream2TableExample {
 }
 //(
 //`word` STRING,
-//`count` INT
+//`frequency` BIGINT NOT NULL
 //)
 //(
 //`word` STRING,
-//`count` INT
+//`frequency` BIGINT
 //)
 //(
 //`f0` STRING,
 //`f1` INT
 //)
-//R1:2> +I[hello, 1]
-//R2:2> +I[hello, 1]
-//R2:3> +I[word, 4]
-//R1:4> +I[hello, 1]
-//R1:3> +I[word, 4]
-//R2:4> +I[hello, 1]
+//R1:1> WordCount{word='hello', frequency=1}
+//R1:3> WordCount{word='hello', frequency=1}
+//R1:4> WordCount{word='word', frequency=4}
+//R2:1> WordCount{word='word', frequency=4}
 //R3:2> +I[hello, 1]
-//R3:3> +I[word, 4]
+//R2:4> WordCount{word='hello', frequency=1}
 //R3:4> +I[hello, 1]
+//R3:3> +I[word, 4]
+//R2:2> WordCount{word='hello', frequency=1}
