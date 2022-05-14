6d50b3eedad226bc08731d1656dd0e57afb70a18
sjf0115 <1203745031@qq.com>
2022-05-14 16:45:58 +0800

    MOD: HyperLogLogSimpleExample

---
+++ redis-example/src/main/java/com/redis/example/hyperloglog/HyperLogLogSimpleExample.java
@@ -0,0 +1,11 @@
+package com.redis.example.hyperloglog;
+/**
+ * 功能：HyperLogLog 简单示例
+ * 作者：SmartSi
+ * 博客：http://smartsi.club/
+ * 公众号：大数据生态
+ * 日期：2022/5/14 下午4:24
+ */
+public class HyperLogLogSimpleExample {
+
+}
