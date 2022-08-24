684456308fa3d529166d63d4d97db73d775b6bf4
sjf0115 <1203745031@qq.com>
2022-08-24 09:17:04 +0800

    MOD: KafkaEndToEndExactlyOnceExample

--- flink-example/src/main/java/com/flink/example/stream/connector/kafka/KafkaEndToEndExactlyOnceExample.java
+++ flink-example/src/main/java/com/flink/example/stream/connector/kafka/KafkaEndToEndExactlyOnceExample.java
@@ -2,17 +2,26 @@ package com.flink.example.stream.connector.kafka;
 
 import org.apache.flink.api.common.functions.FlatMapFunction;
 import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.api.common.restartstrategy.RestartStrategies;
 import org.apache.flink.api.common.serialization.SimpleStringSchema;
+import org.apache.flink.api.common.time.Time;
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer;
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaProducer;
-import org.apache.flink.streaming.connectors.kafka.internals.KeyedSerializationSchemaWrapper;
+import org.apache.flink.streaming.connectors.kafka.KafkaSerializationSchema;
 import org.apache.flink.util.Collector;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import javax.annotation.Nullable;
+import java.nio.charset.StandardCharsets;
 import java.util.Properties;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
 /**
  * 功能：Kafka End To End ExactlyOnce 示例
@@ -22,8 +31,19 @@ import java.util.Properties;
  * 日期：2022/8/23 上午8:43
  */
 public class KafkaEndToEndExactlyOnceExample {
+
+    private static final Logger LOG = LoggerFactory.getLogger(KafkaEndToEndExactlyOnceExample.class);
+
     public static void main(String[] args) throws Exception {
         final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+        // Checkpoint 设置
+        env.enableCheckpointing(60*1000);
+        // 重启与恢复策略
+        env.setRestartStrategy(RestartStrategies.fixedDelayRestart(
+                3, // 最多重启次数
+                Time.of(10, TimeUnit.SECONDS) // 每次重启的时间间隔
+        ));
 
         // Kafka Source
         Properties consumerProps = new Properties();
@@ -42,9 +62,17 @@ public class KafkaEndToEndExactlyOnceExample {
         // 单词计数
         SingleOutputStreamOperator<String> resultStream = sourceStream
                 .flatMap(new FlatMapFunction<String, Tuple2<String, Integer>>() {
+                    private Random random = new Random();
                     @Override
                     public void flatMap(String value, Collector out) {
                         for (String word : value.split("\\s")) {
+                            int num = random.nextInt(5);
+                            if (num == 3) {
+                                System.out.println("[word] " + word + " 异常");
+                                throw new RuntimeException("模拟随机异常");
+                            } else {
+                                System.out.println("[word] " + word + " 正常");
+                            }
                             out.collect(Tuple2.of(word, 1));
                         }
                     }
@@ -61,14 +89,25 @@ public class KafkaEndToEndExactlyOnceExample {
         // Kafka Producer
         Properties producerProps = new Properties();
         producerProps.put("bootstrap.servers", "localhost:9092");
-        //producerProps.put("group.id", "end-to-end-exactly-once-group");
+        producerProps.put("transaction.timeout.ms", "5000");
 
         String producerTopic = "word-count-output";
+        KafkaSerializationSchema<String> serializationSchema = new KafkaSerializationSchema<String>() {
+            @Override
+            public ProducerRecord<byte[], byte[]> serialize(String element, @Nullable Long timestamp) {
+                return new ProducerRecord<>(producerTopic, element.getBytes(StandardCharsets.UTF_8)
+                );
+            }
+        };
+
         FlinkKafkaProducer<String> producer = new FlinkKafkaProducer<>(
-                producerTopic, new KeyedSerializationSchemaWrapper<>(new SimpleStringSchema()),
+                producerTopic, serializationSchema,
                 producerProps, FlinkKafkaProducer.Semantic.EXACTLY_ONCE
         );
         resultStream.addSink(producer);
+
+        // Print 输出 为了作对比
+        resultStream.print();
 
         env.execute();
     }
