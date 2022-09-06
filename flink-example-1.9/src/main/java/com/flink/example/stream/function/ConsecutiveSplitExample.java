bf127582e0add0ec8e697a3c7c8925516285e06a
sjf0115 <1203745031@qq.com>
2022-09-07 07:51:36 +0800

    MOD: ConsecutiveSplitExample

--- flink-example-1.9/src/main/java/com/flink/example/stream/function/ConsecutiveSplitExample.java
+++ flink-example-1.9/src/main/java/com/flink/example/stream/function/ConsecutiveSplitExample.java
@@ -1,5 +1,6 @@
 package com.flink.example.stream.function;
 
+import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.streaming.api.collector.selector.OutputSelector;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
@@ -12,13 +13,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * 功能：Split 分流示例
+ * 功能：连续拆分流
  * 作者：SmartSi
  * CSDN博客：https://smartsi.blog.csdn.net/
  * 公众号：大数据生态
- * 日期：2022/9/6 下午10:51
+ * 日期：2022/9/6 下午11:48
  */
-public class SplitExample {
+public class ConsecutiveSplitExample {
     private static final Logger LOG = LoggerFactory.getLogger(SplitExample.class);
 
     public static void main(String[] args) throws Exception {
@@ -46,13 +47,37 @@ public class SplitExample {
 
         // 偶数流
         DataStream<Integer> evenStream = splitStream.select("EVEN");
-        // 奇数流
-        DataStream<Integer> oddStream = splitStream.select("ODD");
-
         // 输出
         evenStream.print("偶数");
-        oddStream.print("奇数");
 
-        env.execute("SplitExample");
+        // 拆分奇数流 小于5的一个流 大于等于5的一个流
+        DataStream<Integer> oddStream = splitStream.select("ODD");
+        SplitStream<Integer> oddSplitStream = oddStream
+                .map(new MapFunction<Integer, Integer>() {
+                    @Override
+                    public Integer map(Integer num) throws Exception {
+                        return num;
+                    }
+                })
+                .split(new OutputSelector<Integer>() {
+                    @Override
+                    public Iterable<String> select(Integer num) {
+                        // 给数据打标
+                        List<String> tags = new ArrayList<>();
+                        if (num < 5) {
+                            // 小于5
+                            tags.add("LESS");
+                        } else {
+                            //大于等于5
+                            tags.add("MORE");
+                        }
+                        return tags;
+                    }
+                });
+        // 输出
+        oddSplitStream.select("LESS").print("小于");
+        oddSplitStream.select("MORE").print("大于等于");
+
+        env.execute("ConsecutiveSplitExample");
     }
 }
