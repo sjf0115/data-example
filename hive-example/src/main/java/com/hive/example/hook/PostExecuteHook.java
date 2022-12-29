967ef2832c8bc95281a4ed9e24e71cf3757e7f76
sjf0115 <1203745031@qq.com>
2022-12-29 22:40:30 +0800

    MOD: ExecuteWithHookContext

---
+++ hive-example/src/main/java/com/hive/example/hook/PostExecuteHook.java
@@ -0,0 +1,27 @@
+package com.hive.example.hook;
+
+import org.apache.hadoop.hive.ql.hooks.LineageInfo;
+import org.apache.hadoop.hive.ql.hooks.PostExecute;
+import org.apache.hadoop.hive.ql.hooks.ReadEntity;
+import org.apache.hadoop.hive.ql.hooks.WriteEntity;
+import org.apache.hadoop.hive.ql.session.SessionState;
+import org.apache.hadoop.security.UserGroupInformation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Set;
+
+/**
+ * 功能：使用 PostExecute 实现 Task 执行后调用 Hook
+ * 作者：SmartSi
+ * CSDN博客：https://smartsi.blog.csdn.net/
+ * 公众号：大数据生态
+ * 日期：2022/12/29 下午5:28
+ */
+public class PostExecuteHook implements PostExecute {
+    private static final Logger LOG = LoggerFactory.getLogger(PostExecuteHook.class);
+    @Override
+    public void run(SessionState ss, Set<ReadEntity> inputs, Set<WriteEntity> outputs, LineageInfo lInfo, UserGroupInformation ugi) throws Exception {
+        LOG.info("[PostExecute] Command: {}", ss.getLastCommand());
+    }
+}
---
+++ hive-example/src/main/java/com/hive/example/hook/PostExecuteWithHookContextHook.java
@@ -0,0 +1,40 @@
+package com.hive.example.hook;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import org.apache.hadoop.hive.ql.QueryPlan;
+import org.apache.hadoop.hive.ql.hooks.ExecuteWithHookContext;
+import org.apache.hadoop.hive.ql.hooks.HookContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Objects;
+
+/**
+ * 功能：使用 ExecuteWithHookContext 实现 Task 执行后调用 Hook
+ * 作者：SmartSi
+ * 博客：http://smartsi.club/
+ * 公众号：大数据生态
+ * 日期：2021/10/24 下午9:31
+ */
+public class PostExecuteWithHookContextHook implements ExecuteWithHookContext {
+    private static final Logger LOG = LoggerFactory.getLogger(PostExecuteWithHookContextHook.class);
+    private static final Gson gson = new GsonBuilder().create();
+    @Override
+    public void run(HookContext context) throws Exception {
+        HookContext.HookType hookType = context.getHookType();
+        // Task 执行之后调用
+        if (!Objects.equals(hookType, HookContext.HookType.POST_EXEC_HOOK)) {
+            return;
+        }
+        // 执行计划
+        QueryPlan plan = context.getQueryPlan();
+        // 操作名称
+        String operationName = plan.getOperationName();
+        // 查询
+        String query = plan.getQueryString();
+
+        LOG.info("[ExecuteWithHookContext] OperationName: {}", operationName);
+        LOG.info("[ExecuteWithHookContext] Query: {}", query);
+    }
+}
