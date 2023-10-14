c152b051060ce25b9aa58e28f281b998b51a8f23
sjf0115 <1203745031@qq.com>
2023-10-14 15:27:09 +0800

    MOD: RDD 与 DataFrame 交互 - 使用编程方式指定 Schema

--- spark-example-3.1/src/main/java/com/spark/example/sql/DataFrameProgrammaticSchemaExample.java
+++ spark-example-3.1/src/main/java/com/spark/example/sql/DataFrameProgrammaticSchemaExample.java
@@ -1,12 +1,8 @@
 package com.spark.example.sql;
 
-import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
-import com.spark.example.bean.Person;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.function.Function;
 import org.apache.spark.api.java.function.MapFunction;
@@ -15,21 +11,19 @@ import org.apache.spark.sql.types.DataTypes;
 import org.apache.spark.sql.types.StructField;
 import org.apache.spark.sql.types.StructType;
 
-import static org.apache.spark.sql.functions.col;
-
 /**
- * 功能：
+ * 功能：RDD 与 DataFrame 交互 - 使用编程方式指定 Schema
  * 作者：SmartSi
  * CSDN博客：https://smartsi.blog.csdn.net/
  * 公众号：大数据生态
  * 日期：2023/10/11 07:29
  */
-public class JavaSparkSQLExample {
+public class DataFrameProgrammaticSchemaExample {
 
     public static void main(String[] args) throws AnalysisException {
         SparkSession spark = SparkSession
                 .builder()
-                .appName("Java Spark SQL basic example")
+                .appName("programmatic schema example")
                 .master("local[*]")
                 .getOrCreate();
 
@@ -39,15 +33,19 @@ public class JavaSparkSQLExample {
     }
 
     private static void runProgrammaticSchemaExample(SparkSession spark) {
-        // Create an RDD
+        // 1. 原始 RDD
         JavaRDD<String> peopleRDD = spark.sparkContext()
-                .textFile("examples/src/main/resources/data/people.txt", 1)
+                .textFile("spark-example-3.1/src/main/resources/data/people.csv", 1)
                 .toJavaRDD();
 
-        // The schema is encoded in a string
-        String schemaString = "name age";
+        // 2. 将原始 RDD 转化为 Rows 的 RDD
+        JavaRDD<Row> rowRDD = peopleRDD.map((Function<String, Row>) record -> {
+            String[] attributes = record.split(",");
+            return RowFactory.create(attributes[0], attributes[1].trim());
+        });
 
-        // Generate the schema based on the string of schema
+        // 3. 创建 Schema
+        String schemaString = "name age";
         List<StructField> fields = new ArrayList<>();
         for (String fieldName : schemaString.split(" ")) {
             StructField field = DataTypes.createStructField(fieldName, DataTypes.StringType, true);
@@ -55,23 +53,16 @@ public class JavaSparkSQLExample {
         }
         StructType schema = DataTypes.createStructType(fields);
 
-        // Convert records of the RDD (people) to Rows
-        JavaRDD<Row> rowRDD = peopleRDD.map((Function<String, Row>) record -> {
-            String[] attributes = record.split(",");
-            return RowFactory.create(attributes[0], attributes[1].trim());
-        });
-
-        // Apply the schema to the RDD
+        // 4. 将 Schema 应用到 RDD 上创建 DataFrame
         Dataset<Row> peopleDataFrame = spark.createDataFrame(rowRDD, schema);
 
-        // Creates a temporary view using the DataFrame
+        // 在 DataFrame 上创建临时视图
         peopleDataFrame.createOrReplaceTempView("people");
 
-        // SQL can be run over a temporary view created using DataFrames
+        // 运行 SQL
         Dataset<Row> results = spark.sql("SELECT name FROM people");
 
-        // The results of SQL queries are DataFrames and support all the normal RDD operations
-        // The columns of a row in the result can be accessed by field index or by field name
+        // 通过下标获取 name 字段
         Dataset<String> namesDS = results.map(
                 (MapFunction<Row, String>) row -> "Name: " + row.getString(0),
                 Encoders.STRING());
@@ -83,6 +74,5 @@ public class JavaSparkSQLExample {
         // |   Name: Andy|
         // | Name: Justin|
         // +-------------+
-        // $example off:programmatic_schema$
     }
 }
