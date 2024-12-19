3ff9f4a5a6efcb4668b59906e7cffd314570be0e
sjf0115 <1203745031@qq.com>
2024-12-19 23:27:31 +0800

    MOD: MySQLSinkExample

---
+++ spark-example-3.1/src/main/java/com/spark/example/streaming/connector/mysql/DruidConfig.java
@@ -0,0 +1,43 @@
+package com.spark.example.streaming.connector.mysql;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.Serializable;
+
+/**
+ * 功能：Druid 连接池配置类
+ * 作者：SmartSi
+ * CSDN博客：https://smartsi.blog.csdn.net/
+ * 公众号：大数据生态
+ * 日期：2024/12/19 23:02
+ */
+public class DruidConfig implements Serializable {
+
+    private static final Logger LOG = LoggerFactory.getLogger(DruidConfig.class);
+
+    public static DruidDataSource getDataSource() {
+        LOG.info("getDataSource...........");
+        DruidDataSource dataSource = new DruidDataSource();
+
+        // 配置数据库连接参数
+        dataSource.setUrl("jdbc:mysql://localhost:3306/test?useSSL=false&serverTimezone=UTC");
+        dataSource.setUsername("root");
+        dataSource.setPassword("root");
+
+        // 配置连接池参数
+        dataSource.setInitialSize(5); // 初始化连接数
+        dataSource.setMinIdle(5);     // 最小空闲连接数
+        dataSource.setMaxActive(20);  // 最大活跃连接数
+        dataSource.setMaxWait(60000); // 获取连接的最大等待时间（毫秒）
+
+        // 其他可选配置
+        dataSource.setTimeBetweenEvictionRunsMillis(60000); // 连接池检查连接的间隔时间
+        dataSource.setMinEvictableIdleTimeMillis(300000);  // 连接池中连接空闲的最小时间
+        dataSource.setValidationQuery("SELECT 1");         // 验证连接是否有效的 SQL 语句
+        dataSource.setTestWhileIdle(true);                 // 空闲时是否进行连接的验证
+
+        return dataSource;
+    }
+}
--- spark-example-3.1/src/main/java/com/spark/example/streaming/connector/mysql/MySQLSinkExample.java
+++ spark-example-3.1/src/main/java/com/spark/example/streaming/connector/mysql/MySQLSinkExample.java
@@ -1,5 +1,6 @@
 package com.spark.example.streaming.connector.mysql;
 
+import com.alibaba.druid.pool.DruidDataSource;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
@@ -7,6 +8,12 @@ import org.apache.spark.api.java.function.VoidFunction;
 import org.apache.spark.streaming.Durations;
 import org.apache.spark.streaming.api.java.JavaReceiverInputDStream;
 import org.apache.spark.streaming.api.java.JavaStreamingContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 /**
  * 功能：输出到 MySQL
@@ -16,6 +23,7 @@ import org.apache.spark.streaming.api.java.JavaStreamingContext;
  * 日期：2024/12/17 22:46
  */
 public class MySQLSinkExample {
+    private static final Logger LOG = LoggerFactory.getLogger(MySQLSinkExample.class);
     private static String hostName = "localhost";
     private static int port = 9100;
 
@@ -24,29 +32,43 @@ public class MySQLSinkExample {
         JavaSparkContext sparkContext = new JavaSparkContext(conf);
         JavaStreamingContext ssc = new JavaStreamingContext(sparkContext, Durations.seconds(10));
 
-
         // 以端口 9100 作为输入源创建 DStream
         JavaReceiverInputDStream<String> wordStream = ssc.socketTextStream(hostName, port);
 
         wordStream.foreachRDD(new VoidFunction<JavaRDD<String>>() {
             @Override
             public void call(JavaRDD<String> rdd) throws Exception {
+                LOG.info("foreachRDD 遍历 RDD ...");
+                if (rdd.isEmpty()) {
+                    return;
+                }
+
+                // 获取Druid数据源
+                DruidDataSource dataSource = DruidConfig.getDataSource();
+
                 // 遍历 RDD
                 rdd.foreach(new VoidFunction<String>() {
                     @Override
-                    public void call(String s) throws Exception {
-
+                    public void call(String record) throws Exception {
+                        LOG.info("foreach 遍历 RDD 中每条记录 ...");
+                        String[] params = record.split(",");
+                        try (Connection connection = dataSource.getConnection();
+                             PreparedStatement stmt = connection.prepareStatement("INSERT INTO tb_user (id, name, age, email) VALUES (?, ?, ?, ?)")) {
+                            // 设置参数并执行插入操作
+                            stmt.setInt(1, Integer.parseInt(params[0]));
+                            stmt.setString(2, params[1]);
+                            stmt.setInt(3, Integer.parseInt(params[2]));
+                            stmt.setString(4, params[3]);
+                            stmt.executeUpdate();
+                        } catch (SQLException e) {
+                            e.printStackTrace();
+                        }
                     }
                 });
             }
         });
 
-
         ssc.start();
         ssc.awaitTermination();
-    }
-
-    private void createConnection() {
-
     }
 }
