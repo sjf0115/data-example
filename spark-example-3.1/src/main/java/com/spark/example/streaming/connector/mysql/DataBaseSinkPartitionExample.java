fe5f34ceb143796295c2f5cb809ad29990ccf807
sjf0115 <1203745031@qq.com>
2024-12-21 23:49:24 +0800

    MOD: DataBaseSinkPartitionExample

--- spark-example-3.1/src/main/java/com/spark/example/streaming/connector/mysql/DataBaseSinkPartitionExample.java
+++ spark-example-3.1/src/main/java/com/spark/example/streaming/connector/mysql/DataBaseSinkPartitionExample.java
@@ -13,6 +13,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.sql.PreparedStatement;
+import java.util.Iterator;
 
 /**
  * 功能：与外部存储系统交互 反例 出现 object not serializable 异常
@@ -37,40 +38,44 @@ public class DataBaseSinkPartitionExample {
         wordStream.foreachRDD(new VoidFunction<JavaRDD<String>>() {
             @Override
             public void call(JavaRDD<String> rdd) throws Exception {
-                rdd.foreach(new VoidFunction<String>() {
+                rdd.foreachPartition(new VoidFunction<Iterator<String>>() {
                     @Override
-                    public void call(String record) throws Exception {
-                        LOG.info("record：" + record);
+                    public void call(Iterator<String> iterator) throws Exception {
                         // 1. 通过连接池获取连接
                         DruidDataSource dataSource = DruidConfig.getDataSource();
                         DruidPooledConnection connection = dataSource.getConnection();
-
-                        // 2. 遍历 RDD 通过连接与外部存储系统交互
-                        String[] params = record.split(",");
-                        String sql = "INSERT INTO tb_user (id, name, age, email) VALUES (?, ?, ?, ?)";
-                        PreparedStatement stmt = null;
-                        try {
-                            stmt = connection.prepareStatement(sql);
-                            // 设置参数并执行插入操作
-                            stmt.setInt(1, Integer.parseInt(params[0]));
-                            stmt.setString(2, params[1]);
-                            stmt.setInt(3, Integer.parseInt(params[2]));
-                            stmt.setString(4, params[3]);
-                            stmt.executeUpdate();
-                        } catch (Exception e) {
-                            LOG.error("与外部存储系统交互失败：" + e.getMessage());
-                        } finally {
-                            // 3. 关闭连接
-                            if (stmt != null) {
-                                stmt.close();
+                        LOG.info("[INFO] 建立连接");
+                        while (iterator.hasNext()) {
+                            String record = iterator.next();
+                            LOG.info("[INFO] 数据记录：" + record);
+                            // 2. 遍历 RDD 通过连接与外部存储系统交互
+                            String[] params = record.split(",");
+                            String sql = "INSERT INTO tb_user (id, name, age, email) VALUES (?, ?, ?, ?)";
+                            PreparedStatement stmt = null;
+                            try {
+                                stmt = connection.prepareStatement(sql);
+                                // 设置参数并执行插入操作
+                                stmt.setInt(1, Integer.parseInt(params[0]));
+                                stmt.setString(2, params[1]);
+                                stmt.setInt(3, Integer.parseInt(params[2]));
+                                stmt.setString(4, params[3]);
+                                stmt.executeUpdate();
+                                LOG.info("[INFO] 通过连接与外部存储系统交互");
+                            } catch (Exception e) {
+                                LOG.error("[ERROR] 与外部存储系统交互失败：" + e.getMessage());
+                            } finally {
+                                if (stmt != null) {
+                                    stmt.close();
+                                }
                             }
-                            if(connection != null) {
-                                connection.close();
-                            }
+                        }
+                        // 3. 关闭连接
+                        if(connection != null) {
+                            connection.close();
+                            LOG.info("[INFO] 关闭连接");
                         }
                     }
                 });
-
             }
         });
 
